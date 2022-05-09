6343ae1ce458020383b1fecfb6869eea662d0830
outranker <outranker222@gmail.com>
2022-05-10 00:32:13 +0900

    updt

--- src/components/OuterWrapper.js
+++ src/components/OuterWrapper.js
@@ -1,7 +1,7 @@
+import { Box } from "@mui/material";
 import React from "react";
-
 const OuterWrapper = ({ children }) => {
-  return <div style={{ height: "100vh", width: "100vw" }}>{children}</div>;
+  return <Box sx={{ height: "100vh", width: "100vw" }}>{children}</Box>;
 };
 
 export default OuterWrapper;
