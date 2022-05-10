f9e1f7cdcb73bf376f79d0bac495a5ca7364b3fe
outranker <outranker222@gmail.com>
2022-05-10 23:35:23 +0900

    flex box in mui

---
+++ src/components/Stars/StarsCard.js
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const StarsCard = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+      }}
+    ></Box>
+  );
+};
+
+export default StarsCard;
